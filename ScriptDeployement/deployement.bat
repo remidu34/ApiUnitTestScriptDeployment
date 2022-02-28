032aed3f23f210ff2d76c51d2cb25e0e4d94df6b
remi.pro <remi.arnaud.pro@outlook.fr>
2022-02-28 18:36:27 +0100

    edit message echo script

--- ScriptDeployement/deployement.bat
+++ ScriptDeployement/deployement.bat
@@ -1,7 +1,7 @@
 @echo off
 title Git download by students of epsi
 
-echo "Bonjour placez ce script sur le bureau puis mettez remidu34 pour l username et ApiUnitTestScriptDeployment pour le projet. Mettez /swagger a votre url pour acceder a l app"
+echo "Bonjour placez ce script sur le bureau puis mettez remidu34 pour l username et ApiUnitTestScriptDeployment pour le projet. Mettez /swagger a votre url (https://localhost:7116) pour acceder a l app"
 set /p user=Username:
 set /p proj=Project Name:
 
